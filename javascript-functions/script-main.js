d2931f2aa14c852a612ed927c788783b3a7a4381
Freddy Chen <77744025+freddycdesign@users.noreply.github.com>
2024-09-18 13:51:18 -0700

    capliize and length functions in console log

--- javascript-functions/script-main.js
+++ javascript-functions/script-main.js
@@ -1,5 +1,20 @@
+/*
 function captilize(text) {
     return console.log(text.toUpperCase());
 }
+  let captilize = function(text) {
+    console.log(text.toUpperCase());
+}
+*/
 
+let captilize = text => {
+    console.log(text.toUpperCase());
+}
 captilize('hello');
+
+
+function length(text) {
+    return console.log(text.length);
+}
+length('hello sadffsd asdf');
+
